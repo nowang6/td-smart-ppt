dbeab9128ea32729ee18e7be2e0e1027ca8e5373
nowang6 <norman6.wang@gmail.com>
2025-10-16 17:33:04 +0800

    Feat: add template

--- frontend/app/(presentation-generator)/presentation/components/SlideContent.tsx
+++ frontend/app/(presentation-generator)/presentation/components/SlideContent.tsx
@@ -1,20 +1,16 @@
 import React, { useEffect, useState, useMemo } from "react";
-import { Loader2, PlusIcon, Trash2, WandSparkles, StickyNote } from "lucide-react";
+import { Loader2, PlusIcon, Trash2, StickyNote } from "lucide-react";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { Textarea } from "@/components/ui/textarea";
-import { SendHorizontal } from "lucide-react";
 import { toast } from "sonner";
-import { PresentationGenerationApi } from "../../services/api/presentation-generation";
 import ToolTip from "@/components/ToolTip";
 import { RootState } from "@/store/store";
 import { useDispatch, useSelector } from "react-redux";
 import {
   deletePresentationSlide,
-  updateSlide,
 } from "@/store/slices/presentationGeneration";
 import { useGroupLayouts } from "../../hooks/useGroupLayouts";
 import { usePathname } from "next/navigation";
@@ -30,7 +26,6 @@ interface SlideContentProps {
 
 const SlideContent = ({ slide, index, presentationId }: SlideContentProps) => {
   const dispatch = useDispatch();
-  const [isUpdating, setIsUpdating] = useState(false);
   const [showNewSlideSelection, setShowNewSlideSelection] = useState(false);
   const { presentationData, isStreaming } = useSelector(
     (state: RootState) => state.presentationGeneration
@@ -40,37 +35,6 @@ const SlideContent = ({ slide, index, presentationId }: SlideContentProps) => {
   const { renderSlideContent, loading } = useGroupLayouts();
   const pathname = usePathname();
 
-  const handleSubmit = async () => {
-    const element = document.getElementById(
-      `slide-${slide.index}-prompt`
-    ) as HTMLInputElement;
-    const value = element?.value;
-    if (!value?.trim()) {
-      toast.error("请先输入提示词再提交");
-      return;
-    }
-    setIsUpdating(true);
-
-    try {
-      trackEvent(MixpanelEvent.Slide_Edit_API_Call);
-      const response = await PresentationGenerationApi.editSlide(
-        slide.id,
-        value
-      );
-
-      if (response) {
-        dispatch(updateSlide({ index: slide.index, slide: response }));
-        toast.success("幻灯片更新成功");
-      }
-    } catch (error: any) {
-      console.error("Error in slide editing:", error);
-      toast.error("Error in slide editing.", {
-        description: error.message || "Error in slide editing.",
-      });
-    } finally {
-      setIsUpdating(false);
-    }
-  };
   const onDeleteSlide = async () => {
     try {
       trackEvent(MixpanelEvent.Slide_Delete_API_Call);
@@ -194,64 +158,6 @@ const SlideContent = ({ slide, index, presentationId }: SlideContentProps) => {
                 <Trash2 className="text-gray-500 text-xl cursor-pointer" />
               </div>
             </ToolTip>
-          )}
-          {!isStreaming && (
-            <div className="absolute top-2 z-20 sm:top-4 hidden md:block left-2 sm:left-4 transition-transform">
-              <Popover>
-                <PopoverTrigger>
-                  <ToolTip content="Update slide using prompt">
-                    <div
-                      className={`p-2 group-hover:scale-105 rounded-lg bg-[#5141e5] hover:shadow-md transition-all duration-300 cursor-pointer shadow-md `}
-                    >
-                      <WandSparkles className="w-4 sm:w-5 h-4 sm:h-5 text-white" />
-                    </div>
-                  </ToolTip>
-                </PopoverTrigger>
-                <PopoverContent
-                  side="right"
-                  align="start"
-                  sideOffset={10}
-                  className="w-[280px] sm:w-[400px] z-20"
-                >
-                  <div className="space-y-4">
-                    <form
-                      className="flex flex-col gap-3"
-                      onSubmit={(e) => {
-                        e.preventDefault();
-                        handleSubmit();
-                      }}
-                    >
-                      <Textarea
-                        id={`slide-${slide.index}-prompt`}
-                        placeholder="Enter your prompt here..."
-                        className="w-full min-h-[100px] max-h-[100px] p-2 text-sm border rounded-lg focus:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
-                        disabled={isUpdating}
-                        onKeyDown={(e) => {
-                          if (e.key === "Enter" && !e.shiftKey) {
-                            e.preventDefault();
-                            handleSubmit();
-                          }
-                        }}
-                        rows={4}
-                        wrap="soft"
-                      />
-                      <button
-                        disabled={isUpdating}
-                        type="submit"
-                        className={`bg-gradient-to-r from-[#9034EA] to-[#5146E5] rounded-[32px] px-4 py-2 text-white flex items-center justify-end gap-2 ml-auto ${isUpdating ? "opacity-70 cursor-not-allowed" : ""
-                          }`}
-                        onClick={() => {
-                          trackEvent(MixpanelEvent.Slide_Update_From_Prompt_Button_Clicked, { pathname });
-                        }}
-                      >
-                        {isUpdating ? "Updating..." : "Update"}
-                        <SendHorizontal className="w-4 sm:w-5 h-4 sm:h-5" />
-                      </button>
-                    </form>
-                  </div>
-                </PopoverContent>
-              </Popover>
-            </div>
           )}
           {/* Speaker Notes */}
           {!isStreaming && slide?.speaker_note && (
